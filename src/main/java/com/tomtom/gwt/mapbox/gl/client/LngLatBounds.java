4631ea7b6776d6a7b1d590ae552537924825645f
dafo <dafo@nl1mcl-506903.ttg.global>
2016-11-08 12:21:53 +0100

    Adding javadocs

--- src/main/java/com/tomtom/gwt/mapbox/gl/client/LngLatBounds.java
+++ src/main/java/com/tomtom/gwt/mapbox/gl/client/LngLatBounds.java
@@ -1,6 +1,5 @@
 package com.tomtom.gwt.mapbox.gl.client;
 
-import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 /**
@@ -12,17 +11,57 @@ public class LngLatBounds {
     public LngLatBounds(LngLat sw, LngLat ne) {
     }
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getSouthWest
+     * @return 
+     */
     public native LngLat getSouthWest();
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getNorthEast
+     * @return 
+     */
     public native LngLat getNorthEast();
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getNorthWest
+     * @return 
+     */
+    public native LngLat getNorthWest();
+    
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getSouthEast
+     * @return 
+     */
+    public native LngLat getSouthEast();
+    
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getCenter
+     * @return 
+     */
     public native LngLat getCenter();
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getWest
+     * @return 
+     */
     public native LngLat getWest();
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getSouth
+     * @return 
+     */
     public native LngLat getSouth();
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getEast
+     * @return 
+     */
     public native LngLat getEast();
     
+    /**
+     * @see https://www.mapbox.com/mapbox-gl-js/api/#LngLatBounds#getNorth
+     * @return 
+     */
     public native LngLat getNorth();
 }
