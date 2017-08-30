6f5adf1358e8a9c30b7b9a76806ad92ddc06744e
dafo <dafo@nl1mcl-506903.ttg.global>
2017-08-30 15:35:31 +0200

    MDW-2212: adding more layout-paint properties. Breaking change renaming builder setters to "widthXXX".

--- src/main/java/com/tomtom/gwt/mapbox/gl/client/layers/style/StyleFunction.java
+++ src/main/java/com/tomtom/gwt/mapbox/gl/client/layers/style/StyleFunction.java
@@ -40,11 +40,13 @@ public class StyleFunction {
         /**
          * Functions are defined in terms of input and output values. A set of one input value and one output value is known as a "stop."
          * @param stops Required (except for identity functions) array.
+         * @return This Builder.
          * @see https://www.mapbox.com/mapbox-gl-js/style-spec/#function-stops
          */
         @JsOverlay
-        public void setStopsArray(Object[][] stops) {
+        public Builder withStopsArray(Object[][] stops) {
             setStops(JSUtils.toDoubleJsArray(stops));
+            return this;
         }
         
         // Using JsArray to avoid unnecessary java stuff in the arrays. 
@@ -55,19 +57,33 @@ public class StyleFunction {
         /**
          * If specified, the function will take the specified feature property as an input. See Zoom Functions and Property Functions for more information.
          * @param propertyName Optional string.
+         * @return This Builder.
          * @see https://www.mapbox.com/mapbox-gl-js/style-spec/#function-property
          */
+        @JsOverlay
+        public Builder withProperty(String propertyName) {
+            setProperty(propertyName);
+            return this;
+        }
+        
         @JsProperty
-        public native void setProperty(String propertyName);
+        private native void setProperty(String propertyName);
 
         /**
          * The exponential base of the interpolation curve. It controls the rate at which the function output increases. 
          * Higher values make the output increase more towards the high end of the range. With values close to 1 the output increases linearly.
          * @param base Optional number. Default is 1.
+         * @return This Builder.
          * @see https://www.mapbox.com/mapbox-gl-js/style-spec/#function-base
          */
+        @JsOverlay
+        public Builder withBase(double base) {
+            setBase(base);
+            return this;
+        }
+        
         @JsProperty
-        public native void setBase(double base);
+        private native void setBase(double base);
 
         /**
          * A value to serve as a fallback function result when a value isn't otherwise available. It is used in the following circumstances:
@@ -79,33 +95,44 @@ public class StyleFunction {
          * If no default is provided, the style property's default is used in these circumstances.
          *
          * @param value The value to serve as a fallback function result when a value isn't otherwise available
+         * @return This Builder.
          * @see https://www.mapbox.com/mapbox-gl-js/style-spec/#function-default
          */
+        @JsOverlay
+        public Builder withDefault(Object value) {
+            setDefault(value);
+            return this;
+        }
+        
         @JsProperty
-        public native void setDefault(Object value);
+        private native void setDefault(Object value);
 
         /**
          * The color space in which colors interpolated. 
          * Interpolating colors in perceptual color spaces like LAB and HCL tend to produce color ramps that look more consistent and produce colors that can be differentiated more easily than those interpolated in RGB space.
          * @param colorSpace Optional enum. One of rgb, lab, hcl.
+         * @return This Builder.
          * @see https://www.mapbox.com/mapbox-gl-js/style-spec/#function-colorSpace
          */
         @JsOverlay
-        public final void setColorSpace(ColorSpace colorSpace) {
+        public Builder withColorSpace(ColorSpace colorSpace) {
             setColorSpace(colorSpace.name());
+            return this;
         }
 
         @JsProperty
         private native void setColorSpace(String colorSpace);
 
         /**
-         * 
+         * Sets the type of this function.
          * @param type Optional enum. One of identity, exponential, interval, categorical.
+         * @return This Builder.
          * @see https://www.mapbox.com/mapbox-gl-js/style-spec/#function-type
          */
         @JsOverlay
-        public final void setType(FunctionType type) {
+        public Builder withType(FunctionType type) {
             setType(type.name());
+            return this;
         }
 
         @JsProperty
